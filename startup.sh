4899d7d16a9fad8ad0a655dcb488b12584804d05
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-15 01:58:11 +0800

    西元2025年02月15日 (週六) 01時58分11秒 CST

---
+++ startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+full_path=$(realpath "$0")
+base_dir=$(dirname "$full_path")
+cd "$base_dir"
+
+touch .env
+
+# ============
+
+#./cloudflared.sh &
+
+# ============
+
+docker-compose --env-file ./default.env --env-file .env -f ./dify/docker/docker-compose.yaml -f ./dify2openai/docker-compose.yml -f ./cloudflare/docker-compose.yml down
+docker-compose --env-file ./default.env --env-file .env -f ./dify/docker/docker-compose.yaml -f ./dify2openai/docker-compose.yml -f ./cloudflare/docker-compose.yml up --build
+
+# docker-compose --env-file ./default.env --env-file .env -f ./dify2openai/docker-compose.yml up --build
+
